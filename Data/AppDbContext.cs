8c97a404b40f439027ec6abee976e95e302651a2
ssplinterot <margobolshakova49@gmail.com>
2025-12-03 20:24:02 +0300

    +

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Kurs_ArendOff.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs_ArendOff.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("name=ArendConnection")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<AppDbContext>());
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<Place> Places { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Создаём админа при первом запуске
+            var adminHash = BCrypt.Net.BCrypt.HashPassword("admin123");
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Login = "admin", PasswordHash = adminHash, FullName = "Администратор", Role = "Admin" }
+            );
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs_ArendOff.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required] 
+        public string Login { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty; // Храним хэш пароля
+
+        public string FullName { get; set; } = "Пользователь";
+        public string Role { get; set; } = "User"; // User / Admin
+    }
+}
